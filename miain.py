9fdb773c7dcc8734abd22474d9fc8e3440323002
Waratta <waratthaya31@gmail.com>
2025-02-27 18:46:56 +0700

    init

--- miain.py
+++ miain.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.title("Hello")
-st.write("tt")
+Machine_Learning_page = st.Page("Machine_Learning.py", title="Machine Learning", icon=":material/circle:")
+Neural_Network_page = st.Page("Neural_Network.py", title="Neural Network", icon=":material/circle:")
+demo_Machine_Learning_page = st.Page("demo_ML.py", title="demo Machine Learning", icon=":material/circle:")
+demo_Neural_Network_page = st.Page("demo_N.py", title="demo Neural Network", icon=":material/circle:")
+
+pg = st.navigation([Machine_Learning_page, Neural_Network_page,demo_Machine_Learning_page,demo_Neural_Network_page])
+pg.run()
